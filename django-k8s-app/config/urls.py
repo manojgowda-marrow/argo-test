d6a5afb1db07ca1915e9e21e6300a497832cd7b8
manojgowda-marrow <manoj.gowda@dailyrounds.org>
2026-02-11 16:14:08 +0530

    sdfsd

--- django-k8s-app/config/urls.py
+++ django-k8s-app/config/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from api.views import health, name
 
 urlpatterns = [
-    path('health/', health, name='health'),
+    path('health/', health, name='health v2'),
     path('name/', name, name='name'),
 ]
