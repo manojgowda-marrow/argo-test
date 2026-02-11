6c94b100d36f40cc5e5e292ca66bdbaa2349702d
manojgowda-marrow <manoj.gowda@dailyrounds.org>
2026-02-11 12:28:18 +0530

    shd

--- django-k8s-app/api/views.py
+++ django-k8s-app/api/views.py
@@ -4,9 +4,9 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def health(request):
-    return Response({'status': 'healthy v1'})
+    return Response({'status': 'healthy: v2'})
 
 
 @api_view(['GET'])
 def name(request):
-    return Response({'name': 'Django K8s App, argo: version 3.1 '})
+    return Response({'name': 'name: v2'})
